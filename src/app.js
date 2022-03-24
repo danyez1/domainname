7d9582fb9b37dbfe878192d38a1b0c32dc1f44e1
danyez1 <cdanielyanez144@gmail.com>
2022-03-24 00:36:24 +0000

    actualizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoum = ["the", "our", "a"];
+  let adj = ["great", "big", "small", "stronger"];
+  let noum = ["player", "singer", "panther"];
+  for (let pindex = 0; pindex < 3; pindex++) {
+    for (let aindex = 0; aindex < 4; aindex++) {
+      for (let nindex = 0; nindex < 3; nindex++) {
+        console.log(pronoum[pindex] + adj[aindex] + noum[nindex] + ".com");
+      }
+    }
+  }
 };
